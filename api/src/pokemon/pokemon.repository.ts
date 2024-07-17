7e483d2c473efcf0a40608c9b1e97a1ee8d9c29d
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2024-07-16 21:50:43 -0300

    code(domain): repo

---
+++ api/src/pokemon/pokemon.repository.ts
@@ -0,0 +1,29 @@
+import { IPokemon } from "src/@domains/pokemon/domain.interfaces";
+import { IPokemonRepository, IResult } from "src/@domains/pokemon/repository.interface";
+
+export class PokemonRepository implements IPokemonRepository {
+
+    public loadMany = async ({limit, offset}): Promise<IResult> => {
+
+        let url = 'https://pokeapi.co/api/v2/pokemon?' 
+        if(limit){
+            url += `limit=${limit}`
+        }
+
+        if(offset){
+            url += `offset=${offset}`
+        }
+
+        const response = await fetch(url)
+        return await response.json() as IResult
+
+    }
+
+    public findOneById = async (id) : Promise<IPokemon> => {
+        let url = 'https://pokeapi.co/api/v2/pokemon/'+id 
+        const response = await fetch(url)
+        return await response.json() as IPokemon
+
+    }
+
+}
