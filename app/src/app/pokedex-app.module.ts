64886202fe985980bc1ecb1b4ed0cf3996e5cbeb
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2024-07-17 09:24:11 -0300

    partial(generic table)

--- app/src/app/pokedex-app.module.ts
+++ app/src/app/pokedex-app.module.ts
@@ -6,11 +6,13 @@ import { routes } from './pokedex-app.routes';
 import { PokedexAppComponent } from "./pokedex-app.component";
 import { PokemonService } from "./pokemon/pokemon.service";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PokemonTableComponent } from "./pokemon-table/pokemon-table.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    PokemonTableComponent
   ],
   providers: [
     PokemonService,
