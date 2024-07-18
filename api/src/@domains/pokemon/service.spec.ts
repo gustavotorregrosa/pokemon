e7a78ca0f7df0eef79f19ef1dcd34ed91480130a
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2024-07-17 23:04:14 -0300

    test(jest): add jest testing to pokemon service

---
+++ api/src/@domains/pokemon/service.spec.ts
@@ -0,0 +1,30 @@
+import { beforeEach } from "node:test"
+import { IPokemonRepository } from "./repository.interface"
+import { PokemonService } from "./service"
+
+describe('testing pokemon service', () => {
+
+    let repository: IPokemonRepository;
+    let service: PokemonService;
+
+    beforeAll(() => {
+        repository = {
+            findOneById: jest.fn(),
+            loadMany: jest.fn()
+        }
+
+        service = new PokemonService(repository)
+    })
+
+    it('get many', async () => {
+        const pokemonList = await service.loadMany({limit: '', offset: ''});
+        expect(repository.loadMany).toHaveBeenCalled();
+    })
+
+    it('get one', async () => {
+        const pokemonList = await service.findOneById('1')
+        expect(repository.findOneById).toHaveBeenCalled();
+    })
+
+
+})
