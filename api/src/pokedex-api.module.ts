e48245a95561f610fc0c0ce554a03d8d83ddb695
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2024-07-17 23:30:09 -0300

    sec(vars)

--- api/src/pokedex-api.module.ts
+++ api/src/pokedex-api.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonService } from './@domains/pokemon/service';
 import { PokemonRepository } from './pokemon/pokemon.repository';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [PokemonController],
   providers: [
     {
